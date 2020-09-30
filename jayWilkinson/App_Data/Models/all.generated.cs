ae1deeb0a417427ec1d10140d786d030c41e46ed
jay.wilkinson <james.wilkinson@share.co.uk>
2020-09-30 21:01:53 +0000

    Plugin the articulate layout into the main _Layout.cshtml Cleanup

--- jayWilkinson/App_Data/Models/all.generated.cs
+++ jayWilkinson/App_Data/Models/all.generated.cs
@@ -7,7 +7,7 @@ using  Umbraco.Core.Models.PublishedContent;
 using  Umbraco.Web;
 using  Umbraco.ModelsBuilder.Embedded;
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "a0f2b734255e26ee")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.6")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 
 // FILE: models.generated.cs
